af7380f6a7c5c7c1cbe6c103ad513cd328220530
sadys castro <sadyscastro@gmail.com>
2023-05-31 23:43:56 -0600

    componente calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import enUS from 'date-fns/locale/en-US'
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+
 import { Navbar } from '../components/Navbar'
+
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'cumpleaños de mario',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Sadys'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 'calc( 100vh - 80px )' }}
+        />
+      </div>
     </>
   )
 }
