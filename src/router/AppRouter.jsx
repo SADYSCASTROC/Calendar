8d78e7ce0d48778f5633ec4bd2a733709f1b3a55
sadys castro <sadyscastro@gmail.com>
2023-05-31 22:27:10 -0600

    creando componente navbar

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+export const AppRouter = () => {
+
+    const authStatus = 'autenticated'; // not-autenticated
+
+    return (
+        <Routes>
+            {
+                (authStatus === 'not-autenticated')
+                    ? <Route path="/auth/*" element={<LoginPage />} />
+                    : <Route path="/*" element={<CalendarPage />} />
+            }
+
+            <Route path="/*" element={<Navigate to='/auth/login' />} />
+
+        </Routes>
+    )
+}
